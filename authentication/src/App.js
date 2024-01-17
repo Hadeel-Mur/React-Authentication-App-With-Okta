16c986f25740da677659a8777feab9dcda918a7e
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2024-01-17 22:47:24 +0100

    creating the navbar and create routes for the home and the staff pages

---
+++ authentication/src/App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/staff" element={<Staff />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
