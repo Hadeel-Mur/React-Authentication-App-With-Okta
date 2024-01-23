9dee1cfc94d4f7eb3c50d38bfc195c67add0efe7
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2024-01-23 14:33:27 +0100

    create sign in widget file

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -1,22 +1,46 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/staff" element={<Staff />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+import './App.css';
+
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="dev-83080281.okta.com/oauth2/default"
+          client_id="0oaeic58tlUyppOSQ5d7"
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-409495.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
